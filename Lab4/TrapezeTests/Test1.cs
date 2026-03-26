f84c9ce8e118741da568771b7892e35b501cf41a
sandykdanik17 <danik2007.ds10@gmail.com>
2026-03-26 11:22:08 +0200

    <додано unit тест до завдання 1, та внесено зміни у головний файл для запуску програми.>

---
+++ Lab4/TrapezeTests/Test1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab4; 
+
+namespace Lab4.Tests
+{
+    [TestClass]
+    public class TrapezeTests
+    {
+        [TestMethod]
+        public void Indexer_WhenIndexIsZero_ShouldReturnA()
+        {
+            Trapeze t = new Trapeze(10, 20, 5, 1);
+
+            int actualValue = t[0];
+
+            Assert.AreEqual(10, actualValue, "Індексатор повернув неправильне значення для t[0]!");
+        }
+
+        [TestMethod]
+        public void OperatorMultiply_ShouldMultiplyAAndH()
+        {
+            Trapeze t = new Trapeze(5, 7, 4, 1);
+            int scalar = 3;
+
+            Trapeze result = t * scalar;
+
+            
+            Assert.AreEqual(15, result[0], "Поле 'a' не помножилося!"); 
+            Assert.AreEqual(7, result[1], "Поле 'b' мало залишитися без змін!"); 
+            Assert.AreEqual(12, result[2], "Поле 'h' не помножилося!"); 
+            Assert.AreEqual(1, result[3], "Колір 'c' мав залишитися без змін!"); 
+        }
+    }
+}
